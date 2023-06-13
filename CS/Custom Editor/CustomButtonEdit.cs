89698868c69a645762dded28aa3c026e788a9989
NadezhdaDX <37904867+NadezhdaDX@users.noreply.github.com>
2023-06-13 15:45:36 +0400

    Add files via upload

---
+++ CS/Custom Editor/CustomButtonEdit.cs
@@ -0,0 +1,29 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButtonEditImages {
+    [ToolboxItem(true)]
+    public class CustomButtonEdit : ButtonEdit {
+        static CustomButtonEdit() {
+            RepositoryItemCustomButtonEdit.RegisterCustomButtonEdit();
+        }
+
+        public CustomButtonEdit() {
+        }
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public new RepositoryItemCustomButtonEdit Properties {
+            get { return base.Properties as RepositoryItemCustomButtonEdit; }
+        }
+
+        public override string EditorTypeName {
+            get { return RepositoryItemCustomButtonEdit.CustomEditName; }
+        }
+
+    }
+}
---
+++ CS/Custom Editor/CustomButtonEditPainter.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.Utils.Drawing;
+using DevExpress.XtraEditors.Controls;
+using DevExpress.XtraEditors.Drawing;
+using DevExpress.XtraEditors.ViewInfo;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButtonEditImages {
+    public class CustomButtonEditPainter : ButtonEditPainter {
+        static CustomButtonEditPainter() {
+        }
+        public CustomButtonEditPainter() {
+        }
+        protected override void DrawButton(ButtonEditViewInfo viewInfo, EditorButtonObjectInfoArgs info) {
+            RepositoryItemCustomButtonEdit item = viewInfo.Item as RepositoryItemCustomButtonEdit;
+            CustomEditorButtonObjectInfoArgs args = info as CustomEditorButtonObjectInfoArgs;
+            args.SetCustomImage(item);
+            base.DrawButton(viewInfo, info);
+        }
+    }
+}
